392f2d6665af4c1a5efae0158dc2c3ece289f0b9
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 06:51:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,22 +2,13 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |attributes, hash|
-    hash.each do |attribute, names|
+  data.each do |attribute, hash|
+    hash.each do |value, names|
       names.each do |name|
         new_hash[name] ||= {}
         new_hash[name][attribute] ||= []
-        new_hash[name][attribute]
+        new_hash[name][attribute] << value.to_s
       end 
-    end 
-  end
-  data.each do |attribute, hash|
-    hash.each do |value, arr_of_names|
-      new_hash.each do |name, empty|
-        if arr_of_names.include?(name)
-          new_hash[name][attribute].push(value.to_s)
-        end 
-      end
     end 
   end
   new_hash
